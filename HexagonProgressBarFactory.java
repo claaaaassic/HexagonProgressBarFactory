3e9d0d30b8fe1a94a20f96a9a4d74d08b290ef5b
claaaaassic <johnny84131@gmail.com>
2019-11-28 23:25:06 +0800

    Refactor

--- HexagonProgressBarFactory.java
+++ HexagonProgressBarFactory.java
@@ -8,12 +8,14 @@ class HexagonProgressBarFactory {
 
 	private int baseEdgeLen, widthOfProgressBar, percentage;
 	private List<Number> interval;
+	private List<List<Double>> base;
 
 	public HexagonProgressBarFactory(int baseEdgeLen, int widthOfProgressBar, int percentage) {
 		this.baseEdgeLen = baseEdgeLen;
 		this.widthOfProgressBar = widthOfProgressBar;
 		this.percentage = percentage;
 		this.interval = getInterval();
+		this.base = generateCoordinateOfBase();
 	}
 
 	private static final double SQRT3 = Math.sqrt(3);
@@ -34,15 +36,15 @@ class HexagonProgressBarFactory {
 		List<List<Double>> coordinate = new LinkedList<List<Double>>();
 		double halfWidth = widthOfProgressBar / 2;
 
-		coordinate.add(Arrays.asList(SQRT3 / 2 * baseEdgeLen, 0 + halfWidth));
-		coordinate.add(Arrays.asList(SQRT3 * baseEdgeLen - SQRT3 / 2 * halfWidth, baseEdgeLen / 2 + halfWidth / 2));
-		coordinate.add(Arrays.asList(SQRT3 * baseEdgeLen - SQRT3 / 2 * halfWidth, baseEdgeLen * 1.5 - halfWidth / 2));
-		coordinate.add(Arrays.asList(SQRT3 / 2 * baseEdgeLen, baseEdgeLen * 2 - halfWidth));
-		coordinate.add(Arrays.asList(SQRT3 / 2 * halfWidth, 1.5 * baseEdgeLen - halfWidth / 2));
-		coordinate.add(Arrays.asList(SQRT3 / 2 * halfWidth, 0.5 * baseEdgeLen + halfWidth / 2));
+		coordinate.add(Arrays.asList(base.get(0).get(0), base.get(0).get(1) + halfWidth));
+		coordinate.add(Arrays.asList(base.get(1).get(0) - SQRT3 / 2 * halfWidth, base.get(1).get(1) + halfWidth / 2));
+		coordinate.add(Arrays.asList(base.get(2).get(0) - SQRT3 / 2 * halfWidth, base.get(2).get(1) - halfWidth / 2));
+		coordinate.add(Arrays.asList(base.get(3).get(0), base.get(3).get(1) - halfWidth));
+		coordinate.add(Arrays.asList(base.get(4).get(0) + SQRT3 / 2 * halfWidth, base.get(4).get(1) - halfWidth / 2));
+		coordinate.add(Arrays.asList(base.get(5).get(0) + SQRT3 / 2 * halfWidth, base.get(4).get(1) + halfWidth / 2));
 
 		// for calculate interval use
-		coordinate.add(Arrays.asList(SQRT3 / 2 * baseEdgeLen, 0 + halfWidth));
+		coordinate.add(Arrays.asList(base.get(0).get(0), base.get(0).get(1) + halfWidth));
 
 		return generateCoordinateOfProgressBar(coordinate);
 	}
@@ -51,37 +53,36 @@ class HexagonProgressBarFactory {
 		List<List<Double>> coordinate = new LinkedList<List<Double>>();
 		double halfWidth = widthOfProgressBar / 2;
 
-		coordinate.add(Arrays.asList(SQRT3 / 2 * baseEdgeLen, 0 - halfWidth));
-		coordinate.add(Arrays.asList(SQRT3 * baseEdgeLen + SQRT3 / 2 * halfWidth, baseEdgeLen / 2 - halfWidth / 2));
-		coordinate.add(Arrays.asList(SQRT3 * baseEdgeLen + SQRT3 / 2 * halfWidth, baseEdgeLen * 1.5 + halfWidth / 2));
-		coordinate.add(Arrays.asList(SQRT3 / 2 * baseEdgeLen, baseEdgeLen * 2 + halfWidth));
-		coordinate.add(Arrays.asList(-SQRT3 / 2 * halfWidth, 1.5 * baseEdgeLen + halfWidth / 2));
-		coordinate.add(Arrays.asList(-SQRT3 / 2 * halfWidth, 0.5 * baseEdgeLen - halfWidth / 2));
+		coordinate.add(Arrays.asList(base.get(0).get(0), base.get(0).get(1) - halfWidth));
+		coordinate.add(Arrays.asList(base.get(1).get(0) + SQRT3 / 2 * halfWidth, base.get(1).get(1) - halfWidth / 2));
+		coordinate.add(Arrays.asList(base.get(2).get(0) + SQRT3 / 2 * halfWidth, base.get(2).get(1) + halfWidth / 2));
+		coordinate.add(Arrays.asList(base.get(3).get(0), base.get(3).get(1) + halfWidth));
+		coordinate.add(Arrays.asList(base.get(4).get(0) - SQRT3 / 2 * halfWidth, base.get(4).get(1) + halfWidth / 2));
+		coordinate.add(Arrays.asList(base.get(5).get(0) - SQRT3 / 2 * halfWidth, base.get(4).get(1) - halfWidth / 2));
 
 		// for calculate interval use
-		coordinate.add(Arrays.asList(SQRT3 / 2 * baseEdgeLen, 0 - halfWidth));
+		coordinate.add(Arrays.asList(base.get(0).get(0), base.get(0).get(1) - halfWidth));
 
 		return generateCoordinateOfProgressBar(coordinate);
 	}
 
 	private List<List<Double>> generateCoordinateOfProgressBar(List<List<Double>> coordinate) {
 		List<List<Double>> rtn = new LinkedList<List<Double>>();
-		if (Double.parseDouble(interval.get(0) + "") == 0) {
-			return rtn;
-		}
-		rtn.add(coordinate.get(0));
-		for (int i = 0; i < interval.size(); i++) {
-			Double thisInterval = Double.parseDouble(interval.get(i) + "");
-			if (thisInterval == 1) {
-				rtn.add(coordinate.get(i + 1));
-			} else if (thisInterval > 0) {
-				Double oriX = coordinate.get(i).get(0);
-				Double oriY = coordinate.get(i).get(1);
-				Double nextX = coordinate.get(i + 1).get(0);
-				Double nextY = coordinate.get(i + 1).get(1);
-				Double diffX = nextX - oriX;
-				Double diffY = nextY - oriY;
-				rtn.add(Arrays.asList(oriX + thisInterval * diffX, oriY + thisInterval * diffY));
+		if (Double.parseDouble(interval.get(0) + "") != 0) {
+			rtn.add(coordinate.get(0));
+			for (int i = 0; i < interval.size(); i++) {
+				Double thisInterval = Double.parseDouble(interval.get(i) + "");
+				if (thisInterval == 1) {
+					rtn.add(coordinate.get(i + 1));
+				} else if (thisInterval > 0) {
+					Double oriX = coordinate.get(i).get(0);
+					Double oriY = coordinate.get(i).get(1);
+					Double nextX = coordinate.get(i + 1).get(0);
+					Double nextY = coordinate.get(i + 1).get(1);
+					Double diffX = nextX - oriX;
+					Double diffY = nextY - oriY;
+					rtn.add(Arrays.asList(oriX + thisInterval * diffX, oriY + thisInterval * diffY));
+				}
 			}
 		}
 		return rtn;
@@ -125,7 +126,7 @@ class HexagonProgressBarFactory {
 	}
 
 	void print() {
-		String baseCoordinate = format(generateCoordinateOfBase());
+		String baseCoordinate = format(base);
 		String innerCoordinate = format(generateCoordinateOfInnerProgressBar());
 		String outerCoordinate = formatT(generateCoordinateOfOuterProgressBar());
 		System.out.println(interval);
@@ -134,7 +135,7 @@ class HexagonProgressBarFactory {
 	}
 
 	public static void main(String[] args) {
-		HexagonProgressBarFactory svg = new HexagonProgressBarFactory(30, 4, 20);
+		HexagonProgressBarFactory svg = new HexagonProgressBarFactory(30, 4, 36);
 		svg.print();
 	}
 }
